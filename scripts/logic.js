ed75d298674b22a7834b91ffecb2396feb3c9f61
Danny <dhayek92@gmail.com>
2025-04-15 10:55:09 +0300

    Confirmed that average is being calculated correctly in generateReport

--- scripts/logic.js
+++ scripts/logic.js
@@ -4,11 +4,26 @@ const students = [
     {name: "Bob", scores: [77, 81, 99]},
     {name: "Frank", scores: [89, 78, 67]},
     {name: "John", scores: [45, 20, 100]},
-]
+];
 
-// function generateReport(students) {
-//     const studentReports = []
-//     for (let i = 0; i < students.length; i++) {
+function generateReport(students) {
+    const studentReports = [];
+    
+    for (let i = 0; i < students.length; i++) {
+        let avg = 0;
+        let sum = 0;
+        let num = 0;
+        let grade = "F";
+        let curStudent = students[i];
 
-//     }
-// }
+        for (let j = 0; j < curStudent.scores.length; j++) {
+            sum += curStudent.scores[j];
+            num++;
+        }
+        
+        avg = sum / num;
+        console.log(`${avg}`);
+    }
+}
+
+generateReport(students);
