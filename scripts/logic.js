22cee96b3796451c1fb2d2e54d90dff8a17d9edb
Danny <dhayek92@gmail.com>
2025-04-15 10:26:30 +0300

    Created sample data to be used for generating reports

---
+++ scripts/logic.js
@@ -0,0 +1,14 @@
+const students = [
+    {name: "Jeff", scores: [45, 20, 100]},
+    {name: "Ahmad", scores: [70, 88, 63]},
+    {name: "Bob", scores: [77, 81, 99]},
+    {name: "Frank", scores: [89, 78, 67]},
+    {name: "John", scores: [45, 20, 100]},
+]
+
+// function generateReport(students) {
+//     const studentReports = []
+//     for (let i = 0; i < students.length; i++) {
+
+//     }
+// }
